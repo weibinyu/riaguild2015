51c1e824125026185e09bf08098b23099e654240
elinnilsson <elinhejnilsson@gmail.com>
2015-11-03 21:32:59 +0100

    Added the user Elin Nilsson (swoot1) to the guild.

--- src/data/members.js
+++ src/data/members.js
@@ -20,7 +20,8 @@ var members = {
 	ek222mw: require("./users/ek222mw.json"),
 	me222rs: require("./users/me222rs.json"),
 	Angamanga: require("./users/Angamanga.json"),
-	mn22nw: require("./users/mn22nw.json")
+	mn22nw: require("./users/mn22nw.json"),
+	swoot1: require("./users/swoot1.json")
 };
 
 var _ = require("lodash");
