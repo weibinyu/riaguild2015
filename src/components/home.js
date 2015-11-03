45bddbdfd7c4c2895bac759a7a85f5f7a3a9ea90
KungD <david@krawaller.se>
2015-11-03 18:14:42 +0100

    fixed spaces

--- src/components/home.js
+++ src/components/home.js
@@ -12,10 +12,7 @@ var Home = React.createClass({
 			<div>
 				<h3>Welcome!</h3>
 				<p>
-This is the student roster for the 
-<a href="https://coursepress.lnu.se/kurs/ria-utveckling-med-javascript/" target="_blank">RIA JavaScript course</a> 
-at <a href="http://lnu.se" target="_blank">Linnaeus University</a>. Github repo is
-<a href="https://github.com/krawaller/riaguild2015" target="_blank">here</a>.
+This is the student roster for the <a href="https://coursepress.lnu.se/kurs/ria-utveckling-med-javascript/" target="_blank">RIA JavaScript course</a> at <a href="http://lnu.se" target="_blank">Linnaeus University</a>. Github repo is <a href="https://github.com/krawaller/riaguild2015" target="_blank">here</a>.
 				</p>
 				<p>
 The purpose of the guild is to easily find the classmates' projects, and also see their blog posts and pull requests.
