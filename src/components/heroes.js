79c4507f24b4d7706b7e6bd95a2fc9460cd2a400
KungD <david@krawaller.se>
2015-11-04 08:48:07 +0100

    added missing file

---
+++ src/components/heroes.js
@@ -0,0 +1,36 @@
+
+var React = require("react"),
+	_ = require("lodash"),
+	Badge = require("./badge"),
+	mem = require("../data/members"),
+	members = mem.members,
+	actions = mem.actions;
+
+var Heroes = React.createClass({
+	render: function(){
+		var blogger = members[mem.mostposts],
+			helper = members[mem.mostprs];
+		return (
+			<div>
+				<p>These are the current heroes of the guild. Make sure to pay them proper reverence in the Slack channel!</p>
+				<table>
+					<thead>
+						<tr><th>Who</th><th>What</th></tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td><Badge id={blogger.id}/></td>
+							<td>is the most prolific <strong>blogger</strong> with {blogger.blogposts.length} posts!</td>
+						</tr>
+						<tr>
+							<td><Badge id={helper.id}/></td>
+							<td>is the most <strong>helpful</strong> with {helper.pullrequests.length} pull requests!</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		);
+	}
+});
+
+module.exports = Heroes;
