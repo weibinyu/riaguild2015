fa2dba79545dc1c54bebb9aa901b69eac0ea800d
weibinyu <a1994218@hotmail.com>
2015-11-12 21:30:01 +0800

    add comma

--- src/data/members.js
+++ src/data/members.js
@@ -33,7 +33,7 @@ var members = {
 	Janste: require("./users/Janste.json"),
 	as223my: require("./users/as223my.json"),
 	mattiaslj: require("./users/mattiaslj.json"),
-	ea222pu: require("./users/ea222pu.json")
+	ea222pu: require("./users/ea222pu.json"),
 	weibinyu: require("./users/weibinyu.json")
 };
 
