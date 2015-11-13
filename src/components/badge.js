19d22e59bb5c78aa13c8bf50e543ee1c6b6d24ca
afrxx09 <afrxx09@student.lnu.se>
2015-11-13 13:45:27 +0100

    2 fixes: member json-file typo. Can now render PR to non-guild member repo

--- src/components/badge.js
+++ src/components/badge.js
@@ -5,22 +5,32 @@ var React = require("react"),
 
 var Badge = React.createClass({
 	render() {
-		var currentMember = members[this.props.id],
-			names = currentMember.name.split(" "),
+		var	currentMember = members[this.props.id];
+		if(currentMember){
+			var names = currentMember.name.split(" "),
 			surnameletters = {
 				David: 1,
 				Jesper: 2,
 				Mattias: 1,
 				Emil: 1
 			}[names[0]];
-		return (
-			<span className="badge">
-				<Link to={"/member/"+currentMember.github}>
-					<Icon icon={currentMember.icon} />
-					<p className="badge-name">{names[0]+(surnameletters ? " "+names[1].substr(0,surnameletters) : "")}</p>
-				</Link>
-			</span>
-		);
+			return (
+				<span className="badge">
+					<Link to={"/member/"+currentMember.github}>
+						<Icon icon={currentMember.icon} />
+						<p className="badge-name">{names[0]+(surnameletters ? " "+names[1].substr(0,surnameletters) : "")}</p>
+					</Link>
+				</span>
+			);
+		}
+		else {
+			return (
+				<span className="badge">
+					<Icon icon={'elves-wood$sylph'} />
+					<p className="badge-name">Non-guild member repo</p>
+				</span>
+			);
+		}
 	}
 });
 
